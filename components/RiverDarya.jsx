71994e0a8e07bf75496ee4c6ea3b32c79ba4260d
HajiAli <senayousofiali@gmail.com>
2024-11-04 16:24:52 +0430

    added: a carousel fro hero section

--- components/RiverDarya.jsx
+++ components/RiverDarya.jsx
@@ -4,17 +4,17 @@ const AnimatedText = () => {
     return (
         <section className="flex flex-col items-center justify-center h-screen bg-gradient-to-b  relative overflow-hidden">
             <div className="loader text-center f">
-                <h1 className="text-6xl md:text-[10em] font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 animate-text">
+                <h1 className="text-4xl md:text-[10em] text-gray-800 font-bold bg-gradient-to-r from-green-200 to-green-400 animate-text">
                     Darya Cargo
                 </h1>
 
             </div>
 
-            <div className=" mt-10 flex items-center justify-center">
+            <div className=" mt-20 flex items-center justify-center">
                 {['Reliable', 'Global', 'Logistics', 'Solutions'].map((item, index) => (
                     <h2 
                         key={index} 
-                        className="text-2xl md:mx-5 mx-2 md:text-6xl font-semibold text-white"
+                        className="text-xl md:mx-5 mx-1 md:text-6xl font-semibold text-gray-800"
                     >
                         {item}
                     </h2>
